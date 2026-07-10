242f29dcdb710f70666313fa9dba9e49108059f0
latte798 <latte0798@gmail.com>
2026-07-10 15:45:32 +0900

    feat: 부서 속성 수정 기능 추가

--- src/main/java/com/project/hrbank/exception/DepartmentExisted.java
+++
@@ -1,7 +0,0 @@
-package com.project.hrbank.exception;
-
-public class DepartmentExisted extends BaseException {
-    public DepartmentExisted(String message,String detail) {
-        super(message,detail);
-    }
-}
